41128f6633ae1eb9844907e4e057807b2b5a2ce1
Ana Letícia Cabral do Rosário <cabraldorosarioanaleticia@gmail.com>
2025-08-25 08:53:08 -0300

    Cria CRUD para a entidade Carro

---
+++ CRUD_03/app/app/src/main/java/app/service/CarroService.java
@@ -0,0 +1,43 @@
+package app.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import app.entity.Carro;
+import app.repository.CarroRepository;
+
+@Service
+public class CarroService {
+
+	@Autowired
+	private CarroRepository carroRepository;
+	
+	public String save(Carro carro) {
+		this.carroRepository.save(carro);
+		return "Carro salvo com sucesso!";
+	}
+
+	public String update(Carro carro, long id) {
+		carro.setId(id);
+		this.carroRepository.save(carro);
+		return "Carro foi atualizado com sucesso!";
+	}
+
+	public String delete(long id) {
+		this.carroRepository.deleteById(id);
+		return "Carro deletado com sucesso!";
+	}
+
+	public List<Carro> findAll() {
+		List<Carro> lista = this.carroRepository.findAll();
+		return lista;
+		
+	}
+
+	public Carro findById(long id) {
+		Carro carro = this.carroRepository.findById(id).get();
+		return carro;
+	}
+}
